e8fb5a6e7a6376ea1ae66e641f9aa029c3f0a055
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 07:55:30 -0500

    Upload from Lambda

---
+++ js/index.js
@@ -0,0 +1,9 @@
+{
+    "heroImage": "images/index1.jpg",
+    "showcaseImage": "images/index2.jpg",
+    "featureIcons": [
+        "global-network",
+        "quality-assurance",
+        "timely-delivery"
+    ]
+}

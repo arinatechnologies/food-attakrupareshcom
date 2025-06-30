3c7ea08b33b0388aa69c75e24cc981f401b6f352
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-06-30 07:56:03 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,10 @@
+{
+  "about_images": [
+    "food warehouse interior with sacks of grains",
+    "group of food industry professionals meeting",
+    "closeup of various spices in bowls",
+    "modern food storage facility with temperature control",
+    "team of food suppliers in uniform",
+    "quality inspection of food products"
+  ]
+}
